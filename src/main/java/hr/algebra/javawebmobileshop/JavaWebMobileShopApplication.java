991724e6c828240ddf9e6366332872a468ef6ca5
Matej Galić <matej.galic.ets@gmail.com>
2024-06-12 12:36:48 +0200

    init

---
+++ src/main/java/hr/algebra/javawebmobileshop/JavaWebMobileShopApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.javawebmobileshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebMobileShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebMobileShopApplication.class, args);
+    }
+
+}
