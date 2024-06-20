cbcb32cd8a1d091760ec01c6839cb4cd8480c14b
Matej Galić <matej.galic.ets@gmail.com>
2024-06-20 13:06:14 +0200

    exported keys to props

--- src/main/java/hr/algebra/javawebmobileshop/paypal/PayPalConfig.java
+++ src/main/java/hr/algebra/javawebmobileshop/paypal/PayPalConfig.java
@@ -2,15 +2,19 @@ package hr.algebra.javawebmobileshop.paypal;
 
 import com.paypal.core.PayPalEnvironment;
 import com.paypal.core.PayPalHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PayPalConfig {
+    @Value("${paypal.clientId}")
+    String clientId;
+    @Value("${paypal.clientSecret}")
+    String clientSecret;
+
     @Bean
     public PayPalHttpClient payPalHttpClient() {
-        String clientId = "ASNmE3I-mCFQkqdZ0U_ffe956aaPUYA2NB6TP2vphpMtMUarEkcI_okJVZhmS7qQrmUWRFSnbDGqrV4T";
-        String clientSecret = "ELn5DoKTfKWvZKaOxHvkAhGOmNk1AEkvzwfXwz51mxPL78hN48rrw3BF-GC0IrROH7ZpfcdB2776KJa6";
         PayPalEnvironment environment = new PayPalEnvironment.Sandbox(clientId, clientSecret);
         return new PayPalHttpClient(environment);
     }
